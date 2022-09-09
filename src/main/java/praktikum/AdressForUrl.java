64e4cd9cee7b9aa04be607eaeb2ed9913936a9aa
Mikhail Laptev <lapteff77@yandex,ru>
2022-09-09 08:47:28 +0300

    Diplom_3v.4

--- src/main/java/praktikum/AdressForUrl.java
+++ src/main/java/praktikum/AdressForUrl.java
@@ -2,20 +2,20 @@ package praktikum;
 
 public class AdressForUrl {
 
-    public static String baseAdress = "https://stellarburgers.nomoreparties.site/";
+    public final static String baseAdress = "https://stellarburgers.nomoreparties.site/";
 
-    public static String urlApiUser = "https://stellarburgers.nomoreparties.site/api/auth/user";
+    public final static String urlApiUser = "https://stellarburgers.nomoreparties.site/api/auth/user";
 
-    public static String urlApiLogin = "https://stellarburgers.nomoreparties.site/api/auth/login";
+    public final static String urlApiLogin = "https://stellarburgers.nomoreparties.site/api/auth/login";
 
-    public static String urlForgot = "https://stellarburgers.nomoreparties.site/forgot-password";
+    public final static String urlForgot = "https://stellarburgers.nomoreparties.site/forgot-password";
 
-    public static String urlProfile = "https://stellarburgers.nomoreparties.site/account/profile";
+    public final static String urlProfile = "https://stellarburgers.nomoreparties.site/account/profile";
 
-    public static String urlLogin = "https://stellarburgers.nomoreparties.site/login";
+    public final static String urlLogin = "https://stellarburgers.nomoreparties.site/login";
 
-    public static String urlRegister = "https://stellarburgers.nomoreparties.site/register";
-    public static String urlModalBun = "https://stellarburgers.nomoreparties.site/ingredient/61c0c5a71d1f82001bdaaa6d";
-    public static String urlModalSouse = "https://stellarburgers.nomoreparties.site/ingredient/61c0c5a71d1f82001bdaaa74";
-    public static String urlModalFilling = "https://stellarburgers.nomoreparties.site/ingredient/61c0c5a71d1f82001bdaaa71";
+    public final static String urlRegister = "https://stellarburgers.nomoreparties.site/register";
+    public final static String urlModalBun = "https://stellarburgers.nomoreparties.site/ingredient/61c0c5a71d1f82001bdaaa6d";
+    public final static String urlModalSouse = "https://stellarburgers.nomoreparties.site/ingredient/61c0c5a71d1f82001bdaaa74";
+    public final static String urlModalFilling = "https://stellarburgers.nomoreparties.site/ingredient/61c0c5a71d1f82001bdaaa71";
 }
